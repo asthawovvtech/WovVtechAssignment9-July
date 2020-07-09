1f803e3a424a81967c2155c42249366a7f02f4cb
asthawovvtech <67970284+asthawovvtech@users.noreply.github.com>
2020-07-09 15:51:57 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import RouterComponent from './Routes/Routes'
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <RouterComponent />
+    )
+  }
+};
